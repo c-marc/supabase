9d50adbb8d45adb5396e447c85e1b5aedfb78170
Marc C <mcar176@gmail.com>
2023-02-07 12:20:45 +0100

    Get and put

---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import supabase from "../config/supabaseClient";
+import { useState, useEffect } from "react";
+
+//components
+import SmoothieCard from "../components/smoothieCard";
+
+const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("smoothies") //table
+        .select(); //all
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
+
+  return (
+    <div className="page home">
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order by buttns */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => {
+              return <SmoothieCard key={smoothie.id} smoothie={smoothie} />;
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
